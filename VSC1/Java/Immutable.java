8418b2c680271ed2566a13b7c13b85778d53677e
firoz <firozz0007@gmail.com>
2026-07-17 22:51:40 +0530

    immutability of string

---
+++ VSC1/Java/Immutable.java
@@ -0,0 +1,10 @@
+// package VSC1.Java;
+
+public class Immutable {
+    public static void main(String[] args) {
+           String str="Hello";
+           str=str.concat("world");
+
+           System.out.println(str);
+    }
+}
