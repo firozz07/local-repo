e9a5bbc8a53d55a433f75e5df26972f10f17a20f
firoz <firozz0007@gmail.com>
2026-07-18 00:17:26 +0530

    minimum is sherrrr

---
+++ Java/MinArray.java
@@ -0,0 +1,14 @@
+package Java;
+
+public class MinArray {
+    public static void main(String[] args) {
+        int []arr={2,3,4,69,1};
+        int min=arr[0];
+        for (int i = 0; i < arr.length; i++) {
+            if(arr[i]<min){
+                min=arr[i];
+            }
+        }
+        System.out.println(min);
+    }
+}
