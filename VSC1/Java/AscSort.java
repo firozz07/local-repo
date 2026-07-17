8375474a2ad47cafd6fbecff04dead9a68141a18
firoz <firozz0007@gmail.com>
2026-07-17 22:31:08 +0530

    sorting done

---
+++ VSC1/Java/AscSort.java
@@ -0,0 +1,17 @@
+// package VSC1.Java;
+public class AscSort {
+public static void main(String[] args) {
+    int [] arr={2,8,9,27};
+    for (int i = 0; i < arr.length; i++) {
+        for (int j = 0; j < arr.length; j++) {
+            if(arr[i]<arr[j]){
+                int temp=arr[i];
+                arr[i]=arr[j];
+                arr[j]=temp;
+            }
+        }
+        
+    }
+    for(int k:arr) System.out.print(k+" ");
+}
+}
