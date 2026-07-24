88c1055e2449922b3ec61d361f92981be20ed272
firoz <firozz0007@gmail.com>
2026-07-24 21:45:59 +0530

    todays practical

--- VSC1/Java/ReplaceString.java
+++ VSC1/Java/ReplaceString.java
@@ -1,7 +1,7 @@
 public class ReplaceString {
     public static void main(String[] args) {
-        String str="IiiiiiAMiiiFIROZii";
-        str=str.replaceAll("i"," ");
-        System.out.println(str);
+        String str="Good Night";
+        String replacedString=str.replaceAll("Night"," Morning");
+        System.out.println(replacedString);
     }
 }
